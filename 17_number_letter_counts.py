f73ae73dbced82dd371a0b2e437470a8543a35e6
Wacky Scarwick <scarwick@asus-laptop.madbeagle.com>
2015-03-23 04:42:12 +0800

    added return count of words minus spaces

--- 17_number_letter_counts.py
+++ 17_number_letter_counts.py
@@ -1,6 +1,13 @@
+import numutils
 
 def main():
-	print('Problem #17 - Number Letter Counts')
+	words = ''
+
+	for i in range(1,1001):
+		words = words + numutils.num_to_words(i)
+		# print( numutils.num_to_words(i) )
+
+	print( len(words.replace(" ","")) )
 
 if __name__ == '__main__':
 	main()
